acf95ff3d174e6ab342627d41f99a5026770c4b5
WTS25kt <ishii25kenta@gmail.com>
2024-08-29 10:07:23 +0900

    Reactで作った新ページ追加

---
+++ my-readme-generator/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import ReadmeGenerator from './ReadmeGenerator';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ReadmeGenerator />
+      </header>
+    </div>
+  );
+}
+
+export default App;
